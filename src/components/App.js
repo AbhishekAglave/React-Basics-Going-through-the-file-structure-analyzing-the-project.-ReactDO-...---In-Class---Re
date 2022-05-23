c3486d44f45f06fe617ebe0a65c8158cafb08c45
AnimeBoyYt <abhishekaglawe00@gmail.com>
2022-05-23 22:31:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const Name = () => {
+  return <h1 data-ns-test="project-name">Music Player</h1>
+}
+
+const Description = () => {
+  return <p data-ns-test="project-description">Attractive Music Player Using React Only</p>
+}
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Name/>
+      <Description/>
+    </div>
   )
 }
 
